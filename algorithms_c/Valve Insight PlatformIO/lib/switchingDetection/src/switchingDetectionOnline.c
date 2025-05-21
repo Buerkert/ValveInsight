0560454815304ced846fcf014464c09247151685
nikolai.hlubek <nikolai.hlubek@burkert.com>
2025-05-21 10:13:17 +0200

    Fixed subtaction logic in Online algorithm to avoid continuous sum of tail window

--- algorithms_c/Valve Insight PlatformIO/lib/switchingDetection/src/switchingDetectionOnline.c
+++ algorithms_c/Valve Insight PlatformIO/lib/switchingDetection/src/switchingDetectionOnline.c
@@ -7,7 +7,6 @@
 #define TAIL_WINDOW_SIZE          200
 
 static size_t n;
-static float integral = 0;
 static int32_t valuesSum = 0;
 static int32_t tailSum = 0;
 static int16_t tailWindow[TAIL_WINDOW_SIZE];
@@ -19,7 +18,6 @@ float round2(float x) {
 
 void switchingDetectionOnline_Reset(void) {
 	n = 0;
-	integral = 0;
 	valuesSum = 0;
 	tailSum = 0;
 	memset(tailWindow, 0, sizeof(tailWindow));
@@ -29,20 +27,23 @@ float switchingDetectionOnline_Calculate(int16_t adcValue, float timestep) {
 	// running integral
 	valuesSum += adcValue;
 	
-	// Calculate tail mean
-	// remove oldest value from sum
-	tailSum -= tailWindow[n % TAIL_WINDOW_SIZE];
-	tailWindow[n % TAIL_WINDOW_SIZE] = adcValue;
-	// add new value to sum
-	tailSum += adcValue;
+	if (n < TAIL_WINDOW_SIZE) {
+        tailSum += adcValue;
+        tailWindow[n] = adcValue;
+        ++n;
+        // Not a full tail window acquired yet. Stop here.
+        return 0;
+    } 
+
+    // Calculate tail mean
+    size_t idx = n % TAIL_WINDOW_SIZE;
+    tailSum -= tailWindow[idx];  // remove oldest value from sum
+	tailSum += adcValue;  // add new value to sum
+    tailWindow[idx] = adcValue;  // Update the tail window with the new value
 	++n;
 
-	if (n <= TAIL_WINDOW_SIZE) {
-    // Not a full tail window acquired yet. Stop here.
-    return 0;
-  }
-
-	float currIntegral = timestep * (n - TAIL_WINDOW_SIZE * (float)valuesSum / float(tailSum));
+    float tailMean = (float)tailSum / TAIL_WINDOW_SIZE;
+	float currIntegral = timestep * (n - (float)valuesSum / tailMean);
 
 	currIntegral = fabs(currIntegral);
 	currIntegral = round2(currIntegral);
